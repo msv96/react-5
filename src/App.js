744ca3c8c35f51d291415dcfffab0b061846a397
msv96 <msrini1100@gmail.com>
2021-08-09 20:24:06 +0530

    created navbar and topbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Sidebar from './Sidebar';
+import Topbar from './Topbar';
 
 function App() {
   return (
-    <div className="App" id="wrapper">
-      
-    </div>
+    <Router>
+      <div id="wrapper">
+        <Sidebar></Sidebar>
+        <div id="content-wrapper" class="d-flex flex-column">
+          <div id="content">
+            <Topbar></Topbar>
+            {/* <Switch>
+              <Route path="/" component={  } exact={true}></Route>
+              <Route path="/user" component={  } exact={true}></Route>
+              <Route path="/product" component={  } exact={true}></Route>
+            </Switch> */}
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
